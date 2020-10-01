739c82f5a5ad2d1ae63fd23a78e8c92049a0cb88
Kyokatarz <51933605+Kyokatarz@users.noreply.github.com>
2020-10-01 10:55:44 +0300

    Add jwt auth middleware

---
+++ src/middlewares/auth.ts
@@ -0,0 +1,27 @@
+import jwt from 'jsonwebtoken'
+import { JWT_SECRET } from '../util/secrets'
+import { Request, Response, NextFunction } from 'express'
+import { UnauthorizedError, InternalServerError } from '../helpers/apiError'
+
+type tokenType = {
+  user: {
+    id: string
+  }
+}
+
+export default async (req: Request, res: Response, next: NextFunction) => {
+  //Retrieve token:
+  const token = req.header('x-auth-token')
+
+  try {
+    //No token = bye bye
+    if (!token) throw 'NoToken'
+
+    const decodedPayload = jwt.verify(token, JWT_SECRET) as tokenType
+    req.user = decodedPayload.user // req.user now has user id
+    next()
+  } catch (err) {
+    if (err === 'NoToken') next(new UnauthorizedError('No token found', err))
+    next(new InternalServerError(err))
+  }
+}
