489597fe2d600b0914e9fda23cee2aa9ad5dce6c
Kyokatarz <51933605+Kyokatarz@users.noreply.github.com>
2020-10-01 10:39:43 +0300

    Fix a typo

--- src/util/secrets.ts
+++ src/util/secrets.ts
@@ -14,7 +14,7 @@ export const ENVIRONMENT = process.env.NODE_ENV
 const prod = ENVIRONMENT === 'production' // Anything else is treated as 'dev'
 
 export const SESSION_SECRET = process.env['SESSION_SECRET'] as string
-export const JWT_SECRET = process.env['SESSION_SECRET'] as string
+export const JWT_SECRET = process.env['JWT_SECRET'] as string
 export const MONGODB_URI = (prod
   ? process.env['MONGODB_URI']
   : process.env['MONGODB_URI_LOCAL']) as string
