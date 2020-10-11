a37c4eee071695022ba363a1e6d8f32ed7d591e9
Kyokatarz <51933605+Kyokatarz@users.noreply.github.com>
2020-10-12 02:41:26 +0300

    Update scss

--- client/src/App.tsx
+++ client/src/App.tsx
@@ -1,11 +1,13 @@
 import React from 'react'
 
 import Routes from './Routes'
+import './App.scss'
 
 export default function App() {
   return (
     <>
       <Routes />
+      <div></div>
     </>
   )
 }
--- client/src/index.tsx
+++ client/src/index.tsx
@@ -6,6 +6,7 @@ import { BrowserRouter as Router } from 'react-router-dom'
 import App from './App'
 import * as serviceWorker from './serviceWorker'
 import makeStore from './redux/store'
+import './index.scss'
 
 const store = makeStore()
 
