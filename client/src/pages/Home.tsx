2163379e5fd4a04c458c0c18a491926ca5c5b220
Kyokatarz <51933605+Kyokatarz@users.noreply.github.com>
2020-10-12 02:42:40 +0300

    Changes to Home, will change back later

--- client/src/pages/Home.tsx
+++ client/src/pages/Home.tsx
@@ -1,9 +1,8 @@
 import React, { useEffect, useState } from 'react'
 import { Redirect, useHistory, useLocation } from 'react-router-dom'
-
-import {GoogleLogin} from 'react-google-login'
+import { GoogleLogin } from 'react-google-login'
 import axios from 'axios'
-
+import LibraryHomePage from '../components/LibraryHomePage/index'
 
 type JWTToken = {
   token: string
@@ -13,7 +12,7 @@ const GOOGLE_CLIENT_ID =
   '966391822521-16klvao4ikgokq93vhvs0h6i58encvgk.apps.googleusercontent.com'
 
 export default function Home() {
-  const [redirect, setRedirect] = useState('');
+  const [redirect, setRedirect] = useState('')
   const [loggedIn, setLoggedIn] = useState(false)
   const history = useHistory()
 
@@ -21,33 +20,36 @@ export default function Home() {
     token: string
   }
 
-  const responseGoogle = async (resp:any) => {
-    const res = await axios.post('/api/v1/auth/google', {id_token: resp.tokenId})
-    console.log('res:', res.data.token);
+  const responseGoogle = async (resp: any) => {
+    const res = await axios.post('/api/v1/auth/google', {
+      id_token: resp.tokenId,
+    })
+    console.log('res:', res.data.token)
     //TODO: Do something with token
     history.push('/')
     setLoggedIn(true)
-
   }
 
-  useEffect(()=> {
-    
-  })
-  if (redirect) return <Redirect to={redirect}/>
-
+  useEffect(() => {})
+  if (redirect) return <Redirect to={redirect} />
 
   return (
     <>
-     <h1>Home page</h1>
-
-     <GoogleLogin
-        clientId={GOOGLE_CLIENT_ID}
-        buttonText="Login with Google"
-        onSuccess={responseGoogle}
-        onFailure={responseGoogle}
-        cookiePolicy={'single_host_origin'}
-      />
-      
+      <LibraryHomePage />
     </>
   )
+  // return (
+  //   <L>
+  //    <h1>Home page</h1>
+
+  //    <GoogleLogin
+  //       clientId={GOOGLE_CLIENT_ID}
+  //       buttonText="Login with Google"
+  //       onSuccess={responseGoogle}
+  //       onFailure={responseGoogle}
+  //       cookiePolicy={'single_host_origin'}
+  //     />
+
+  //   </LibraryHomePage>
+  // )
 }
