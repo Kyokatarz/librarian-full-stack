fad0626b56678eee4e3b47b44e8e0895609e94de
Kyokatarz <51933605+Kyokatarz@users.noreply.github.com>
2020-10-12 02:42:01 +0300

    Add Footer

---
+++ client/src/components/Footer/Footer.tsx
@@ -0,0 +1,20 @@
+import React from 'react'
+
+import './Footer.scss'
+
+const Footer = () => {
+  return (
+    <div className="Footer">
+      <p>Librarian &#169; 2020</p>
+      <p>
+        Developed and Design by
+        <a href="https://github.com/Kyokatarz/" target="_blank">
+          {' '}
+          Kyo Tran
+        </a>
+      </p>
+    </div>
+  )
+}
+
+export default Footer
