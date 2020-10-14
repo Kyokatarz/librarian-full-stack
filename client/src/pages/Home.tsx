99d1cb3d95b25aecc44e0c32e4c29d62bb8ee816
Kyokatarz <51933605+Kyokatarz@users.noreply.github.com>
2020-10-14 09:35:30 +0300

    Update Home.tsx

--- client/src/pages/Home.tsx
+++ client/src/pages/Home.tsx
@@ -1,15 +1,7 @@
-import React, { useEffect, useState } from 'react'
-import { Redirect, useHistory, useLocation } from 'react-router-dom'
-
+import React from 'react'
 import axios from 'axios'
+
 import LibraryHomePage from '../components/LibraryHomePage/index'
-
-type JWTToken = {
-  token: string
-}
-
-const GOOGLE_CLIENT_ID =
-  '966391822521-16klvao4ikgokq93vhvs0h6i58encvgk.apps.googleusercontent.com'
 
 export default function Home() {
   return (
