2a990303ff572679d3bf9ef0609df9c1bd9ec520
Kyokatarz <51933605+Kyokatarz@users.noreply.github.com>
2020-10-16 12:04:42 +0300

    Update GoogleLogInBtn.tsx

--- client/src/components/GoogleLogInBtn/GoogleLogInBtn.tsx
+++ client/src/components/GoogleLogInBtn/GoogleLogInBtn.tsx
@@ -1,15 +1,20 @@
 import React from 'react'
 import { GoogleLogin } from 'react-google-login'
 import axios from 'axios'
+import { useDispatch } from 'react-redux'
+import { getUserData } from '../../redux/actions/user'
 
 const GOOGLE_CLIENT_ID =
   '966391822521-16klvao4ikgokq93vhvs0h6i58encvgk.apps.googleusercontent.com'
 
 const GoogleLogInBtn = () => {
+  const dispatch = useDispatch()
   const responseGoogle = async (resp: any) => {
     const res = await axios.post('/api/v1/auth/google', {
       id_token: resp.tokenId,
     })
+    localStorage.setItem('token', res.data.token)
+    dispatch(getUserData(res.data.token))
   }
 
   const failedGoogle = async (resp: any) => {}
